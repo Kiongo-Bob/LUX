7c8d428133901832f38eb44a83ae20cdf22a7072
Kiongo-Bob <Njoguub@gmail.com>
2023-03-20 22:06:29 +0300

    Sentiment Analysis Progess

---
+++ Assigmnets/CSVReduction.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+Tweeps_CSV = pd.read_csv('Tweet_data.csv', encoding='latin1', nrows= 10000)
---
+++ Assigmnets/Week2_Python_Project.py
@@ -0,0 +1,19 @@
+# Python. Given an array of integers, find the sum of its elements.
+import numpy as np
+
+# Generate a random uni-dimensional array of 10 integers like teenagers' ages
+rand_int_arr = np.random.randint(13, 19, 10)
+print(rand_int_arr)
+
+#Getting the sum of the generaed ages using a loop
+sum_loop = 0
+
+for i in rand_int_arr:
+    sum_loop += i
+print(sum_loop)    
+#Getting the sum using sum function
+sum_arr = np.sum(rand_int_arr)
+print(sum_arr)
+
+print(sum_loop == sum_arr)
+
